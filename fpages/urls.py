17c330ba5a72fce6b2f3d2cd780675569daee0b4
Aditi Mittal <aditi.iitr@gmail.com>
2013-02-08 23:13:30 +0530

    Add url for flatepages.

--- fpages/urls.py
+++ fpages/urls.py
@@ -10,8 +10,11 @@ urlpatterns = patterns('',
     # url(r'^fpages/', include('fpages.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'', include('django.contrib.flatpages.urls')),
+
 )
